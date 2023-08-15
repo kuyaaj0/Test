8d02346fa8fbd7567b36e07de755782c96a4f8be
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2023-08-15 08:35:31 +0800

    Add files via upload

---
+++ assets/preload/data/infernum-bside/Cutscene.lua
@@ -0,0 +1,8 @@
+function onCreate()	
+	--recreating og stage
+
+end
+
+function addAnim(tag, anim, frame, loop)
+	addAnimationByPrefix(tag, anim, anim, frame, loop) --Shortcut for addAnimationByPrefix()
+end
